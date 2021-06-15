af3b2163ae8dc6564ed60d2cd068b00ad2b07839
Steven Clark <stevenclark@steb.localdomain>
2021-06-15 15:01:00 -0600

    tutorial 1

---
+++ src/ReactRogue.js
@@ -0,0 +1,43 @@
+import React, { useRef, useEffect, useState } from "react";
+import InputManager from "./InputManager";
+
+const ReactRogue = ({ width, height, tilesize }) => {
+  const canvasRef = React.useRef(null);
+  const [player, setPlayer] = useState({ x: 64, y: 128 });
+  let inputManager = new InputManager();
+  const handleInput = (action, data) => {
+    console.log(`handle input: ${action} ${JSON.stringify(data)}`);
+    let newPlayer = { ...player };
+    newPlayer.x += data.x * tilesize;
+    newPlayer.y += data.y * tilesize;
+    setPlayer(newPlayer);
+  };
+
+  useEffect(() => {
+    console.log("Bind input");
+    inputManager.bindKeys();
+    inputManager.subscribe(handleInput);
+    return () => {
+      inputManager.unbindKeys();
+      inputManager.unsubscribe(handleInput);
+    };
+  });
+
+  useEffect(() => {
+    console.log("Draw");
+    const ctx = canvasRef.current.getContext("2d");
+    ctx.clearRect(0, 0, width * tilesize, height * tilesize);
+    ctx.fillStyle = "#000";
+    ctx.fillRect(player.x, player.y, 16, 16);
+  });
+  return (
+    <canvas
+      ref={canvasRef}
+      width={width * tilesize}
+      height={height * tilesize}
+      style={{ border: "1px solid black" }}
+    ></canvas>
+  );
+};
+
+export default ReactRogue;
