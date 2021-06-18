5ef1a090585a592b55c931a6fdb5fdd51ec67fb7
kylebaugh <kylebaugh1@gmail.com>
2021-06-18 15:08:31 -0600

    redesigned home page to have stats/inventory on left of map and event log/monster data on right of map. Built skeleton for how data will be displayed

--- src/ReactRogue.js
+++ src/ReactRogue.js
@@ -36,8 +36,8 @@ const ReactRogue = ({ width, height, tilesize, atlases }) => {
     newWorld.createCellularMap();
     newWorld.moveToSpace(world.player);
     let spawner = new Spawner(newWorld);
-    spawner.spawnLoot(50);
-    spawner.spawnMonsters(20);
+    spawner.spawnLoot(10);
+    spawner.spawnMonsters(10);
     spawner.spawnStairs();
     setWorld(newWorld);
   }, []);
@@ -57,46 +57,272 @@ const ReactRogue = ({ width, height, tilesize, atlases }) => {
     world.draw(ctx);
   });
   return (
-    <div style={{display:'flex'}}>
-      <canvas
-        ref={canvasRef}
-        width={width * tilesize}
-        height={height * tilesize}
+    <div>
+      <header 
         style={{
-          border: "1px solid SaddleBrown",
-        }}
-      ></canvas>
-      <div>equipped</div>
-      <ul>
-        {world.player.hands.map((item, index) => (
-          <li key={index}>{item[0].attributes.name}</li>
-        ))}
-      </ul>
-      {world.player.inspecting.length === 1 && (
-        <>
-          <div>readied</div>
-          <ul>
-            <li>
-              {
-                world.player.inventory[world.player.inspecting[0]].attributes
-                  .name
-              }
-            </li>
-          </ul>
-        </>
-      )}
-      <div>inventory</div>
-      <ul>
-        {world.player.inventory.map((item, index) => (
-          <li key={index}>{item.attributes.name}</li>
-        ))}
-      </ul>
-      <div>gold: {world.player.gold}</div>
-      <ul>
-        {world.history.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+          display:'flex',
+          justifyContent:'space-evenly',
+          height:'1.5vh',
+          width:'98vw',
+          borderStyle:'solid', 
+          borderColor:'white',
+          alignItems:'center',
+          }}>
+      </header>
+      
+      <div style={{
+        display:'flex',
+        justifyContent:'space-evenly'
+        }}>
+
+        <div className='leftOfCanvas'
+          style={{
+            display:'flex',
+            flexDirection:'column',
+            alignItems:'center',
+            width: '20vw',
+            minHeight:'100vh',
+            borderStyle:'solid',
+            borderColor:'black',
+          }}>
+          
+        <div
+          className='newPlayer'
+          style={{
+            display:'flex',
+            flexDirection:'column',
+            minHeight:'15%',
+            width:'95%',
+            justifyContent:'center',
+            alignItems:'center',
+            borderStyle:'solid',
+            borderColor:'black',
+            marginTop:'1vw',
+          }}
+          >
+            <h3>Player Stats</h3>
+            <section
+                style={{
+                  display:'flex',
+                  justifyContent:'center',
+                  alignItems:'center',
+                  height: '2vh',
+                  width:'27.5%',
+                  paddingLeft:'1vw',
+                  paddingRight:'1vw',
+                  borderStyle:'solid',
+                  borderColor:'black', 
+                  marginTop:'-1vh',
+                  marginBottom:'1vh'
+                }}
+              >Health Bar
+            </section>
+            <div
+            style={{
+              display:'flex', 
+              width:'35%', 
+              justifyContent:'space-between'
+            }}
+            >
+            <div>
+              Attack: 
+              <br></br>
+              Defense: 
+              <br></br>
+              Damage: 
+              <br></br>
+              Armor: 
+              <br></br>
+              Health: 
+              <br></br>
+              Visibility: 
+            </div>
+            <div
+              style={{
+                display:'flex',
+                flexDirection:'column',
+                textAlign:'right',
+              }}
+            >
+              {world.player.attributes.attack}
+              <br></br>
+              {world.player.attributes.defense}
+              <br></br>
+              {world.player.attributes.damage}
+              <br></br>
+              {world.player.attributes.armor}
+              <br></br>
+              {world.player.attributes.health}
+              <br></br>
+              {world.player.attributes.sightRadius}
+              <br></br><br></br>
+
+              </div>
+             
+            </div>
+
+          </div>
+
+          <div
+            className='equippedItems'
+            style={{
+              display:'flex',
+              flexDirection:'column',
+              minHeight:'15%',
+              width:'95%',
+              justifyContent:'center',
+              alignItems:'center',
+              borderStyle:'solid',
+              borderColor:'black',
+              marginTop:'1vw',
+              marginBottom:'1vw',
+
+            }}
+          >
+            <h3>Equipped Items</h3>
+            <ul
+              style={{
+                backgroundColor:'green'
+              }}
+            >
+              {world.player.hands.map((item, index) => (
+                <li key={index}
+                >{item[0].attributes.name}</li>
+                ))}
+            </ul>
+          </div>
+          
+          {world.player.inspecting.length === 1 && (
+            <div
+              className='readiedItem'
+              style={{
+                display:'flex',
+                flexDirection:'column',
+                minHeight:'15%',
+                width:'95%',
+                justifyContent:'center',
+                alignItems:'center',
+                borderStyle:'solid',
+                borderColor:'black',
+                marginBottom:'1vw',
+                }}
+            >
+                <h3>{world.player.inventory[world.player.inspecting[0]].attributes
+                      .name} Readied!</h3>
+                <p>Press "E" to equip, or "R" to remove from Inventory</p>
+            </div>
+          )}
+
+          <div
+            className='fullInventory'
+            style={{
+              display:'flex',
+              flexDirection:'column',
+              minHeight:'15%',
+              width:'95%',
+              justifyContent:'center',
+              alignItems:'center',
+              borderStyle:'solid',
+              borderColor:'black'
+            }}>
+                <h3>Inventory</h3>
+                <ol type='1'>
+                  {world.player.inventory.map((item, index) => (
+                    <li key={index}
+                      style={{backgroundColor:'lightblue'}}
+                    >{item.attributes.name}</li>
+                    ))}
+                </ol>
+                <p>Press Number Key to Ready an Item!</p>
+            </div>
+          
+          </div>
+
+        <div className='leftSide' 
+          style={{
+            display:'flex', 
+            flexDirection:'column',
+
+
+            }}>
+          <canvas
+            ref={canvasRef}
+            width={width * tilesize * 1}
+            height={height * tilesize * 1}
+            maxWidth={'75vw'}
+            style={{
+              border: "1px solid SaddleBrown",
+            }}
+          ></canvas>
+        
+        </div>
+
+        <div className='rightSide'
+          style={{
+            display:'flex',
+            flexDirection:'column',
+            // justifyContent:'space-evenly',
+            alignItems:'center',
+            width:'20vw',
+            height:'100vh',
+            // padding:'1vw',
+            borderStyle:'solid', 
+            borderColor:'black',
+          }}
+          >
+          <div className="eventHistory"
+            style={{
+              height:'40%',
+              width:'95%', 
+              borderStyle:'solid', 
+              borderColor:'black',
+              marginTop:'1vw'
+            }}
+            >
+            <h2 className='eventHeader'
+              style={{
+                display:'flex',
+                justifyContent:'center'
+              }}
+            >Event History</h2>
+            <ul>
+              {world.history.map((item, index) => (
+                <li key={index}>{item}</li>
+                ))}
+            </ul>
+          </div>
+
+          <div className='monsterPicture'
+            style={{
+              display:'flex',
+              height:'20%',
+              width:'95%',
+              borderStyle:'solid',
+              borderColor:'black',
+              alignItems:'center', 
+              justifyContent:'center',
+              marginTop:'1vw'
+
+            }}
+          >Monster Picture</div>
+          <div className='monsterStats'
+            style={{
+              display:'flex',
+              height:'20%',
+              width:'95%',
+              borderStyle:'solid',
+              borderColor:'black',
+              alignItems:'center', 
+              justifyContent:'center',
+              marginTop:'1vw'
+            }}
+          >Monster Stats</div>
+
+        </div>
+      
+      </div>
+
+
     </div>
   );
 };
