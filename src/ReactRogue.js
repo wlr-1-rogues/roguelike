c8cfd39d962e8b55f0a8417d0fd16dbbabd295b4
alex <alexanderstapp@gmail.com>
2021-06-24 14:04:03 -0600

    styling fix

--- src/ReactRogue.js
+++ src/ReactRogue.js
@@ -231,8 +231,118 @@ const ReactRogue = ({ width, height, tilesize, atlases }) => {
               </div>
             </div>
           </div>
-          <InventoryDisplay atlas={atlases.itemAtlas} world={world} />
+
+          <div
+            className="equippedItems"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              minHeight: "15%",
+              width: "95%",
+              justifyContent: "center",
+              alignItems: "center",
+              borderStyle: "solid",
+              borderColor: "black",
+              marginTop: "1vw",
+              marginBottom: "1vw",
+              backgroundColor: "rgba(211, 211, 211, 0.598)",
+            }}
+          >
+            <h3>Equipped Items</h3>
+            {world.player.left.map((item, index) => (
+              <p key={index} style={{ height: 19 }}>
+                6. {item.name}
+              </p>
+            ))}
+            {world.player.right.map((item, index) => (
+              <p key={index} style={{ height: 19 }}>
+                7. {item.name}
+              </p>
+            ))}
+            {world.player.head.map((item, index) => (
+              <p key={index} style={{ height: 19 }}>
+                8. {item.name}
+              </p>
+            ))}
+            {world.player.torso.map((item, index) => (
+              <p key={index} style={{ height: 19 }}>
+                9. {item.name}
+              </p>
+            ))}
           </div>
+
+          {world.player.inspecting.length === 1 && (
+            <div
+              className="readiedItem"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                minHeight: "15%",
+                width: "95%",
+                justifyContent: "center",
+                alignItems: "center",
+                borderStyle: "solid",
+                borderColor: "black",
+                marginBottom: "1vw",
+                backgroundColor: "rgba(211, 211, 211, 0.598)",
+              }}
+            >
+              <h3>{inspecting.item.name} Readied!</h3>
+              <h4>Upon inspecting the {inspecting.item.name} you find...</h4>
+              {inspecting.item.class === "weapon" ? (
+                <div>
+                  <p>Attack +{inspecting.item.mod1}</p>
+                  <p>Damage +{inspecting.item.mod2}</p>
+                </div>
+              ) : inspecting.item.class === "shield" ? (
+                <p>Block + {inspecting.item.mod1}</p>
+              ) : inspecting.item.class === "head" ||
+                inspecting.item.class === "torso" ? (
+                <p>Defense +{inspecting.item.mod1}</p>
+              ) : inspecting.item.class === "healthCon" ? (
+                <p>Health +{inspecting.item.mod1}</p>
+              ) : inspecting.item.class === "shieldCon" ? (
+                <p>Block +{inspecting.item.mod1}</p>
+              ) : (
+                <p>A dusty old tome with strange symbols</p>
+              )}
+
+              {typeof inspecting.pos === "string" ? (
+                <p>Press "Q" to unequip, or "K" to destroy</p>
+              ) : inspecting.item.name === "Tome of Fireball" ? (
+                <p>Press fire direction, or "K" to destroy</p>
+              ) : inspecting.item.class === "healthCon" ||
+                inspecting.item.class === "shieldCon" ? (
+                <p>Press "E" to drink, or "K" to destroy</p>
+              ) : (
+                <p>Press "E" to equip, or "K" to destroy</p>
+              )}
+            </div>
+          )}
+
+          <div
+            className="fullInventory"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              minHeight: "15%",
+              width: "95%",
+              justifyContent: "center",
+              alignItems: "center",
+              borderStyle: "solid",
+              borderColor: "black",
+              backgroundColor: "rgba(211, 211, 211, 0.598)",
+            }}
+          >
+            <h3>Inventory</h3>
+            <ol type="1">
+              {world.player.inventory.map((item, index) => (
+                <li key={index}>{item.name}</li>
+              ))}
+            </ol>
+            <p>Press Number Key to Ready an Item!</p>
+          </div>
+        </div>
         <div
           className="leftSide"
           style={{
@@ -253,15 +363,17 @@ const ReactRogue = ({ width, height, tilesize, atlases }) => {
             }}
           ></canvas>
         </div>
+
         <div
           className="rightSide"
           style={{
-            height: "40%",
-            width: "95%",
-            borderStyle: "solid",
-            borderColor: "black",
-            marginTop: "1vw",
-            backgroundColor: "rgba(211, 211, 211, 0.598)",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            width: "20vw",
+            height: "100vh",
+            // borderStyle: "solid",
+            // borderColor: "black",
           }}
         >
           <div
@@ -272,7 +384,7 @@ const ReactRogue = ({ width, height, tilesize, atlases }) => {
               borderStyle: "solid",
               borderColor: "black",
               marginTop: "1vw",
-              backgroundColor:'rgba(211, 211, 211, 0.598)'
+              backgroundColor: "rgba(211, 211, 211, 0.598)",
             }}
           >
             <h2
