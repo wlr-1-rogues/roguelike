b356389c5d2e1c3b2f8813961e98c19f1d813fb5
alex <alexanderstapp@gmail.com>
2021-06-30 10:03:23 -0600

    fixed new item inspect not being removed when item is destroyed by monster

--- src/ReactRogue.js
+++ src/ReactRogue.js
@@ -138,7 +138,7 @@ const ReactRogue = ({ width, height, tilesize, atlases }) => {
     newWorld.createCellularMap();
     newWorld.moveToSpace(world.player);
     let spawner = new Spawner(newWorld);
-    spawner.spawnLoot(6);
+    spawner.spawnLoot(100);
     spawner.spawnMonsters(100);
     spawner.spawnStairs();
     setWorld(newWorld);
