cad7dcdb4abbeea1f6dcd550f1c89b7b2331625c
alex <alexanderstapp@gmail.com>
2021-06-30 11:48:13 -0600

    reset default values

--- src/ReactRogue.js
+++ src/ReactRogue.js
@@ -138,9 +138,9 @@ const ReactRogue = ({ width, height, tilesize, atlases }) => {
     newWorld.createCellularMap();
     newWorld.moveToSpace(world.player);
     let spawner = new Spawner(newWorld);
-    spawner.spawnLoot(0);
+    spawner.spawnLoot(6);
     spawner.spawnMimicChest();
-    spawner.spawnMonsters(0);
+    spawner.spawnMonsters(100);
     spawner.spawnStairs();
     setWorld(newWorld);
     if(world.showWinScreen === true){
